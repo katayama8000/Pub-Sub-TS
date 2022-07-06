220d7c48260ba9eca000a6b08c503bc03f47ec92
katayama <tattu.0310@gmail.com>
2022-07-06 18:25:29 +0900

    ✨ input機能実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Todo} from "./Todo"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+  todos: Todo[] = [];
+  newTodo: string;
+
+  saveTodo() {
+    if (this.newTodo) {
+      let todo = new Todo()
+      todo.name = this.newTodo
+      todo.isCompleted = true
+      this.todos.push(todo)
+      this.newTodo = "";
+    } else {
+      alert("please Enter Todo")
+    }
+  }
 }
