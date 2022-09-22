dba490df7f6d709d37df3d79220d7f9e460eb15a
katayama <tattu.0310@gmail.com>
2022-09-22 13:40:28 +0900

    ⚡️ add/todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {Todo} from "./Todo"
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
@@ -13,13 +13,21 @@ export class AppComponent {
 
   saveTodo() {
     if (this.newTodo) {
-      let todo = new Todo()
-      todo.name = this.newTodo
-      todo.isCompleted = true
-      this.todos.push(todo)
-      this.newTodo = "";
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = false;
+      this.todos.push(todo);
+      this.newTodo = '';
     } else {
-      alert("please Enter Todo")
+      alert('please Enter Todo');
     }
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  completeTodo(index: number) {
+    this.todos[index].isCompleted = !this.todos[index].isCompleted;
   }
 }
