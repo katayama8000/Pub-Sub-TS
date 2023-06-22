a616681cb99dad84126711f2a33ea23ecd0dd5c2
katayama8000 <tattu.0310@gmail.com>
2023-06-22 12:53:41 +0900

    init

--- src/app/const/meta.const.ts
+++
@@ -1,9 +0,0 @@
-export const meta = {
-  name: 'description',
-  content: 'Angular Todo List',
-  charset: 'UTF-8',
-  httpEquiv: 'Content-Type',
-  scheme: 'UTF-8',
-  url: 'https://angular-todo-list-1e8a3.web.app/',
-  site_name: 'Angular Todo List',
-};
